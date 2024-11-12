3af10981c2e97d11a1fc6453081ae0a613a3b18b
diltung <113580391+diltung@users.noreply.github.com>
2024-11-11 22:12:30 -0700

    Update PuzzleTest.java

--- COMP1502-24/Assign 2/test/unittests/PuzzleTest.java
+++ COMP1502-24/Assign 2/test/unittests/PuzzleTest.java
@@ -2,13 +2,44 @@ package unittests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PuzzleTest {
+import model.Puzzle;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+/**
+ * Test of puzzle class
+ * @author diltung
+ */
+
+class PuzzleTest {
+	private Puzzle puzzle;
+	
+	/**
+	 * Set up an instance of Puzzle class.
+	 */
+	@BeforeEach
+	public void setUp() {
+		puzzle = new Puzzle(1234567890, "Crossword", "PuzzleBrand", 9.99, 5, 12, "Sudoku");
 	}
+
+	/**
+	 * test of setPuzzleType method in puzzle class
+	 */
+	@Test
+	public void testSetPuzzleType() {
+		puzzle.setPuzzleType("Crossword");
+		assertEquals("Crossword", puzzle.getPuzzleType());
+	}
+
+	/**
+	 * test of getAvailableCount method in puzzle class
+	 */
+	@Test
+	public void testGetAvailableCount() {
+		assertEquals(5, puzzle.getAvailableCount());
+	}
+
+	
 
 }
