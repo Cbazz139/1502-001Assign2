95fcd6db697c1fe1dbf88671bc9a4e649afe5fec
diltung <113580391+diltung@users.noreply.github.com>
2024-11-11 23:19:41 -0700

    Update Puzzles.java

--- Assignment2/src/model/Puzzles.java
+++ Assignment2/src/model/Puzzles.java
@@ -1,5 +1,56 @@
 package model;
 
-public class Puzzles extends Toy{
+/**
+ * Represents a Puzzle toy
+ * Inherits from the Toy class.
+ * @author diltung
+ */
+public class Puzzle extends Toy{
+	
+	private String puzzleType;
+
+	/**
+	 * Constructs a Puzzle toy
+	 * 
+	 * @param serialNumber
+	 * @param name
+	 * @param brand
+	 * @param price
+	 * @param availableCount
+	 * @param ageAppropriate
+	 * @param puzzleType
+	 */
+	public Puzzle(long serialNumber, String name, String brand, double price, int availableCount, int ageAppropriate,
+			String puzzleType) {
+		super(serialNumber, name, brand, price, availableCount, ageAppropriate);
+		this.puzzleType = puzzleType;
+	}
+
+	/**
+	 * returns the type of puzzle
+	 * 
+	 * @return type of puzzle
+	 */
+	public String getPuzzleType() {
+		return puzzleType;
+	}
+
+	/**
+	 * sets the type of puzzle
+	 * 
+	 * @param type of puzzle
+	 */
+	public void setPuzzleType(String puzzleType) {
+		this.puzzleType = puzzleType;
+	}
+
+	/**
+	 * Returns a string of the Puzzle toy
+	 */
+	@Override
+	public String toString() {
+		return super.toString() + " Puzzle Type: " + puzzleType;
+
+	}
 
 }
