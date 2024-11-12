1c7d12e30294acdd3c5cd7f5594ffb59f959560b
diltung <113580391+diltung@users.noreply.github.com>
2024-11-11 22:11:34 -0700

    Update FigureTest.java

--- COMP1502-24/Assign 2/test/unittests/FigureTest.java
+++ COMP1502-24/Assign 2/test/unittests/FigureTest.java
@@ -2,13 +2,34 @@ package unittests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class FigureTest {
+import model.Figure;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+/**
+ * Test for figure class
+ * @author diltung
+ */
+class FigureTest {
+	private Figure figure;
+	
+	/**
+	 * Sets up with an instance of the Figure class.
+	 */
+	@BeforeEach
+	public void setUp() {
+		figure = new Figure(1234567890, "Superman", "FigureBrand", 29.99, 5, 6, "Superhero");
 	}
+
+	/**
+	 * test of setClassification method in Figure class
+	 */
+	@Test
+	public void testSetClassification() {
+		figure.setClassification("dilpreet");
+		assertEquals("dilpreet", figure.getClassification());
+	}
+
 
 }
