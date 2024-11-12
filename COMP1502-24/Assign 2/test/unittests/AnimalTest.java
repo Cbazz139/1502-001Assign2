74a8b9edc7eb4275ba6b87c7d0d01bf56634cb27
diltung <113580391+diltung@users.noreply.github.com>
2024-11-11 22:10:00 -0700

    Update AnimalTest.java

--- COMP1502-24/Assign 2/test/unittests/AnimalTest.java
+++ COMP1502-24/Assign 2/test/unittests/AnimalTest.java
@@ -2,6 +2,7 @@ package unittests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.Animal;
@@ -12,10 +13,29 @@ import model.Animal;
  */
 class AnimalTest {
 	private Animal animal;
- 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	@BeforeEach
+	public void setUp() {
+		animal = new Animal(1234567890, "Elephant", "AnimalBrand", 19.99, 10, 3, "dilpreet", "Large");
 	}
+ 
+	/**
+	 * Test for the setMaterial method in the Animal class.
+	 */
+	@Test
+	public void testSetMaterial() {
+		animal.setMaterial("Rubber");
+		assertEquals("Rubber", animal.getMaterial());
+	}
+	
+	/**
+	 * Test for the setSize method in the Animal class.
+	 */
+	@Test
+	public void testSetSize() {
+		animal.setSize("Small");
+		assertEquals("Small", animal.getSize());
+	}
+	
 
 }
