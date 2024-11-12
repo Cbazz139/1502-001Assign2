54daff8887059f0d44689c5f40b8710b2421d7f7
diltung <113580391+diltung@users.noreply.github.com>
2024-11-11 22:10:44 -0700

    Update BoardGameTest.java

--- COMP1502-24/Assign 2/test/unittests/BoardGameTest.java
+++ COMP1502-24/Assign 2/test/unittests/BoardGameTest.java
@@ -2,13 +2,44 @@ package unittests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class BoardGameTest {
+import model.BoardGame;
 
+/**
+ * Test for boardgame class
+ * @author diltung
+ */
+
+class BoardGameTest {
+	private BoardGame boardGame;
+	
+	/**
+	 * Set up an instance of BoardGame class.
+	 */
+	@BeforeEach
+	public void setUp() {
+		boardGame = new BoardGame(1234567890, "Chess", "BoardGameBrand", 19.99, 10, 8, 2, "DesignerName", 2);
+	}
+	
+	/**
+	 * Test for the getmaxplayers method in the BoardGame class.
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testGetMaxPlayers() {
+		assertEquals(2, boardGame.getMaxPlayers());
+	}
+	
+	/**
+	 * Test for the setDesigner method in the BoardGame class.
+	 */
+	@Test
+	public void testGetDesigners() {
+		assertEquals("DesignerName", boardGame.getDesigners());
 	}
 
-}
+	
+	}
+
+
